cc2d6fb2bc86036a10b133d904d0a599f0bada67
Joey Guerra <jguerra@thoughtworks.com>
2019-06-06 15:43:59 -0500

    Replies aren't being sent to the same thread

--- src/bot.js
+++ src/bot.js
@@ -127,14 +127,22 @@ class HangoutsChatBot extends Adapter {
           'When sending a reply, the envelope must contain a message');
     }
     this.postMessage_(
-        this.getSpaceFromEnvelope_(envelope),
+        this.getThreadFromEnvelope_(envelope),
         envelope.message.thread,
         strings[0],
         strings[1],
         envelope.message.httpRes,
         true);
   }
-
+  getThreadFromEnvelope_(envelope) {
+    if(envelope.message){
+      return envelope.message.thread.name;
+    }
+    if(envelope.room) {
+      return envelope.room;
+    }
+    throw new Error("When sending a message, the envelope must have either a message or a room.");
+  }
   /**
    * Gets the space name from the envelope object. The envelope must have either
    * a message or a room. If it has both, the message is used.
